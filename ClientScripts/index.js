e73518c9cbc15e7876af4823ed194fc6fdb91f63
Cyjanek-Potasu <89212967+Cyjanek-Potasu@users.noreply.github.com>
2023-10-24 19:57:52 +0200

    Update The camera Client scripts

--- ClientScripts/index.js
+++ ClientScripts/index.js
@@ -1,18 +1,14 @@
-const express = require('express')
-const app = express();
 const fs = require('fs')
 const port = 3000;
 
 // start capture
 const videoStream = require('./videoStream');
 videoStream.startVideoStream({
-        width: 1920,
-        height: 1080,
-        fps: 24,
+        width: 2592,
+        height: 1944,
+        fps: 10,
         encoding: 'JPEG',
-        quality: 25 // lower is faster, less quality
+        quality: 10 // lower is faster, less quality
     }, 
     '/stream.mjpg', true);
 
-app.use(express.static(__dirname+'/public'));
-app.listen(port, () => console.log(`Example app listening on port ${port}! In your web browser, navigate to http://<IP_ADDRESS_OF_THIS_SERVER>:3000`));
--- ClientScripts/videoStream.js
+++ ClientScripts/videoStream.js
@@ -4,35 +4,52 @@ const raspberryPiCamera = require('raspberry-pi-camera-native');
 let lastFrameObj = {
     lastFrame: null
 };
+let cameraOptions = {
+    width: 2592,
+    height: 1944,
+    fps: 10,
+    encoding: 'JPEG',
+    quality: 10 // lower is faster, less quality
+}
+
+let ws = new WebSocket("ws://192.168.1.11:3000")
 
 let videoStream = {
+    getStatus: () => {
+        //return the status of the web socket
+        if(ws.readyState == WebSocket.OPEN){
+            return true
+        }
+        else{
+            return false
+        }
+    },
     getLastFrame: () => {
         return lastFrameObj.lastFrame;
     },
-    startVideoStream: function (cameraOptions, isVerbose) {
-        // Start capture
-        raspberryPiCamera.start(cameraOptions);
-        if (isVerbose) {
-            console.log('Camera started.');
-        }
+    startVideoStream: function (cameraOptions) {
+        try {
+            raspberryPiCamera.start(cameraOptions);
+            // Start capture
+        
+
 
         // Create a WebSocket client and connect to the remote server
-        const ws = new WebSocket("ws://192.168.1.11:3000");
+        
 
         // WebSocket open event handler
         ws.on('open', () => {
-            if (isVerbose) {
-                console.log('WebSocket connected to remote server.');
-            }
 
+            ws.send("cameraWs");
+
+            
             // Frame handler function
             let frameHandler = (frameData) => {
                 try {
                     ws.send(frameData);
+               
                 } catch (ex) {
-                    if (isVerbose) {
-                        console.log('Unable to send frame over WebSocket: ' + ex);
-                    }
+                    console.log(ex);
                 }
             };
 
@@ -42,12 +59,35 @@ let videoStream = {
             // WebSocket close event handler
             ws.on('close', () => {
                 frameEmitter.removeListener('frame', frameHandler);
-                if (isVerbose) {
-                    console.log('WebSocket connection to remote server closed.');
-                }
+
+                reconnect(); //----------> tries to reconnect through the websocket
             });
         });
+        } catch (er) {
+            
+            throw new Error();
+
+        }
+
     }
 };
 
-module.exports = videoStream;
+// Reconnect function that checks the websocket status and tries to reconnect every 5 seconds
+function reconnect() {
+    if (ws.readyState == WebSocket.CLOSED || ws.readyState == WebSocket.CLOSING) {
+        // Create a new websocket object with the same url and options
+        ws = new WebSocket("ws://192.168.1.11:3000", {protocolVersion: 8, origin: 'http://websocket.org'});
+        // Call the startVideoStream function again to attach the event listeners
+        try {
+            videoStream.startVideoStream(cameraOptions);
+        } catch (er) {
+            console.log("Failing to Connect to Master Server")
+        }
+
+        
+    }
+    // Call the reconnect function again after 5 seconds
+    setTimeout(reconnect, 3000);
+}
+videoStream.startVideoStream();
+
